b5e852d5ebfaf56e758517687a69d3f9906217b0
TrstnSch <tristan.schulz@tu-dortmund.de>
2024-11-29 16:28:39 +0100

    ADD: dropout and init

--- code/PGExplainer/networks.py
+++ code/PGExplainer/networks.py
@@ -18,14 +18,26 @@ class MLP(nn.Module):
 # Three layer GNN   (GCN) for Graph Classification based on PGExplainer Source code
 class GraphGNN(nn.Module):
     def __init__(self, features = 10, labels = 2):
-        super().__init__()
+        super(GraphGNN, self).__init__()
         
-        self.hidden1 = gnn.conv.GraphConv(features, 20)   # GraphConvolution layer1: input dim = #features??, output dim = hidden dim = 20 , ReLu activation, bias = true in og config
+        # TODO: Try GCNConv
+        self.hidden1 = gnn.GraphConv(features, 20)   # GraphConvolution layer1: input dim = #features??, output dim = hidden dim = 20 , ReLu activation, bias = true in og config
+        #nn.init.xavier_normal_(self.hidden1.lin_rel.weight)
+        #nn.init.xavier_normal_(self.hidden1.lin_root.weight)
         self.relu1 = nn.ReLU()
-        self.hidden2 = gnn.conv.GraphConv(20, 20)         # GraphConvolution layer2: input dim = hidden dim = 20,
+        self.dropout1 = nn.Dropout(p=0.1)
+
+        self.hidden2 = gnn.GraphConv(20, 20)         # GraphConvolution layer2: input dim = hidden dim = 20,
+        #nn.init.xavier_normal_(self.hidden2.lin_rel.weight)
+        #nn.init.xavier_normal_(self.hidden2.lin_root.weight)
         self.relu2 = nn.ReLU()
-        self.hidden3 = gnn.conv.GraphConv(20, 20)         # GraphConvolution layer3: 
+        self.dropout2 = nn.Dropout(p=0.1)
+
+        self.hidden3 = gnn.GraphConv(20, 20)         # GraphConvolution layer3: 
+        #nn.init.xavier_normal_(self.hidden3.lin_rel.weight)
+        #nn.init.xavier_normal_(self.hidden3.lin_root.weight)
         self.relu3 = nn.ReLU()
+        self.dropout3 = nn.Dropout(p=0.1)
         
         self.lin = nn.Linear(20*2, labels)                    # fully connected layer(Dense) => nn.linear: input dim = hidden dim * 2 due to concat of pooling, output dim = output dim = classes
 
@@ -56,12 +68,15 @@ class GraphGNN(nn.Module):
     def getNodeEmbeddings(self, x, edge_index):
         emb1 = self.hidden1(x, edge_index)
         emb1 = self.relu1(emb1)
+        emb1 = self.dropout1(emb1)
         
         emb2 = self.hidden2(emb1, edge_index)
         emb2 = self.relu2(emb2)
+        emb2 = self.dropout1(emb2)
         
         emb3 = self.hidden3(emb2, edge_index)
         emb3 = self.relu3(emb3)
+        emb3 = self.dropout1(emb3)
 
         return emb3
     
